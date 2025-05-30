0788aacb4d3e51957c4f88c50a909e1edd58c84c
arakiztain <arakiztain.gaizka@gmail.com>
2025-05-30 13:32:10 +0200

    utils-router

--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-
+import { isLoggedInAPI } from "../middlewares/authMiddleware.js";
+import authRouter from "./authRouter.js";
+import userRouter from "./userRouter.js";
 const router = Router();
 
 router.get("/",(req,res)=>{
     res.send("hola terra")
 })
+
+router.use("/",authRouter);
+router.use("/user", isLoggedInAPI, userRouter);
+
 
 export default router
