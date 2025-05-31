ee8bc2f6ff4f3370c006b945f394d499946653b3
CarlosBor <carlosborjawebdev@gmail.com>
2025-05-31 02:26:33 +0200

    Add basic router, login page with css, server node_modules gitignore

---
+++ client/pages/Login/Login.jsx
@@ -0,0 +1,51 @@
+import styles from "./Login.module.css";
+import icon1 from "../../assets/_Terraforms/Individual/SVG/Punky-Green.svg";
+import icon2 from "../../assets/_Terraforms/Individual/SVG/Mistyk-Blue.svg";
+import icon3 from "../../assets/_Terraforms/Individual/SVG/Melos-Pink.svg";
+import icon4 from "../../assets/_Terraforms/Individual/SVG/Melos-Blue.svg";
+import icon5 from "../../assets/_Terraforms/Individual/SVG/Punky-Lime.svg";
+import icon6 from "../../assets/_Terraforms/Individual/SVG/Boba-Orange.svg";
+
+const Login = () =>{
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log("clickity click");
+    }
+
+    return(
+        <div className={styles.fullscreen}>
+            <div className={styles.title}>
+                <img src={icon1} className="icon" />
+                <img src={icon2} className="icon" />
+                <img src={icon3} className="icon" />
+                <span>terra ripple</span>
+            </div>
+            <div className={styles.formGrid}>
+                <div className={styles.formWrapper}>
+                    <form action={handleSubmit} className={styles.form}>
+                        <div class={styles.field}>
+                            <input className={styles.input} placeholder="" type="email" name="email" required />
+                            <label className={styles.label}>Email</label>
+                        </div>
+                        <div class={styles.field}>
+                            <input className={styles.input} placeholder="" type="password" name="password" required />
+                            <label className={styles.label}>Password</label>
+                        </div>
+                        <button className={styles.formButton} type="submit">Log in!</button>
+                    </form>
+                </div>
+            </div>
+            <div className={styles.lettering}>
+                <h1 class={styles.hello}>Hello</h1>
+                <h1 class={styles.again}>again!</h1>
+                <div className={styles.terraformBottom}>
+                    <img src={icon4} className="icon" />
+                    <img src={icon5} className="icon" />
+                    <img src={icon6} className="icon" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
---
+++ client/src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from '../pages/Login/Login';
+import './global.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
