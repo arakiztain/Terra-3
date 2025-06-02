99f424e23c425814ee1a4b3fed6ad512d457dcee
Carlos <carlosborjawebdev@gmail.com>
2025-06-02 16:46:43 +0200

    Plug Login form into backend

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,13 +1,16 @@
 import styles from "./Login.module.css";
-
+import { useState } from "react";
+import loginFetch from "../../utils/fetchServer";
 import icon4 from "../../assets/_Terraforms/Individual/SVG/Melos-Blue.svg";
 import icon5 from "../../assets/_Terraforms/Individual/SVG/Punky-Lime.svg";
 import icon6 from "../../assets/_Terraforms/Individual/SVG/Boba-Orange.svg";
 
 const Login = () =>{
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const handleSubmit = (e) =>{
         e.preventDefault();
-        console.log("clickity click");
+        loginFetch({email: email, password: password});
     }
 
     return(
@@ -16,11 +19,11 @@ const Login = () =>{
                 <div className={styles.formWrapper}>
                     <form onSubmit={handleSubmit} className={styles.form}>
                         <div class={styles.field}>
-                            <input className={styles.input} placeholder="" type="email" name="email" required />
+                            <input className={styles.input} placeholder="" type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
                             <label className={styles.label}>Email</label>
                         </div>
                         <div class={styles.field}>
-                            <input className={styles.input} placeholder="" type="password" name="password" required />
+                            <input className={styles.input} placeholder="" type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
                             <label className={styles.label}>Password</label>
                         </div>
                         <button className={styles.formButton} type="submit">Log in!</button>
