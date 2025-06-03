24002d0addc614b781dc6e2d264fbac336d194a2
darroyo083 <darroyo083@gmail.com>
2025-06-03 10:36:35 +0200

    FAQ updated

--- client/src/pages/Faq/Faq.jsx
+++ client/src/pages/Faq/Faq.jsx
@@ -1,52 +1,68 @@
-import React, { useState } from 'react';
-import styles from './Faq.module.css';
+import React from 'react';
+import { Accordion, AccordionItem as Item } from "@szhsin/react-accordion";
+import chevronDown from "./chevron-down.svg";
+import styles from "./Faq.module.css";
 
 const faqData = [
   {
     question: "How can I log in?",
-    answer: "To log in, enter your username and password in the corresponding fields and click 'Log in!'.",
+    answer: "To log in, enter your username and password in the corresponding fields and click 'Log in!'."
   },
   {
     question: "What should I do if I forgot my password?",
-    answer: "If you forgot your password, look for the 'Forgot your password?' link on the login page or contact support.",
+    answer: "If you forgot your password, look for the 'Forgot your password?' link on the login page or contact support."
   },
   {
     question: "How can I register?",
-    answer: "If you don't have an account, look for the link to register on the homepage.",
+    answer: "If you don't have an account, look for the link to register on the homepage."
   },
   {
     question: "What should I do if I can't log in?",
-    answer: "Verify that the entered data is correct, ensure caps lock is not enabled, and if necessary, reset your password.",
-  },
+    answer: "Verify that the entered data is correct, ensure caps lock is not enabled, and if necessary, reset your password."
+  }
 ];
 
+/**
+ * @type {React.ExoticComponent<import('@szhsin/react-accordion').AccordionItemProps>}
+ */
+const AccordionItem = ({ header, ...rest }) => (
+  <Item
+    {...rest}
+    header={
+      <>
+        {header}
+        <img className={styles.chevron} src={chevronDown} alt="Chevron Down" />
+      </>
+    }
+    className={styles.item}
+    buttonProps={{
+      className: ({ isEnter }) =>
+        `${styles.itemBtn} ${isEnter && styles.itemBtnExpanded}`
+    }}
+    contentProps={{ className: styles.itemContent }}
+    panelProps={{ className: styles.itemPanel }}
+  />
+);
+
 const Faq = () => {
-  const [openIndex, setOpenIndex] = useState(null);
-
-  const toggleAccordion = (index) => {
-    setOpenIndex(openIndex === index ? null : index);
-  };
-
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Frequently Asked Questions</h1>
-      {faqData.map((item, index) => (
-        <div key={index} className={styles.accordion}>
-          <div
-            className={styles.accordionHeader}
-            onClick={() => toggleAccordion(index)}
-          >
-            {item.question}
-          </div>
-          <div
-            className={`${styles.accordionContent} ${
-              openIndex === index ? styles.open : ""
-            }`}
-          >
-            {item.answer}
-          </div>
-        </div>
-      ))}
+
+      <div className={styles.accordion}>
+        {/* `transitionTimeout` prop should be equal to the transition duration in CSS */}
+        <Accordion transition transitionTimeout={250}>
+          {faqData.map((item, index) => (
+            <AccordionItem 
+              key={index} 
+              header={item.question} 
+              initialEntered={index === 0} // Make first item expanded by default
+            >
+              {item.answer}
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
     </div>
   );
 };
