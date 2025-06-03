199f93765a373e7b3a7541b4d4a633059648682b
Carlos <carlosborjawebdev@gmail.com>
2025-06-03 07:05:47 +0200

    Add work towards admin connection to backend

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import style from './Admin.module.css';
 import ProjectList from '../../components/ProjectList/ProjectList';
 
@@ -23,20 +24,44 @@ const mockData = [
 ];
 
 const Admin = () =>{
+  const [formData, setFormData] = useState({
+    projectName: "",
+    projectUrl: "",
+    description: "",
+    reviewers: [],
+  });
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({
+      ...prev,
+      [name]: name === "reviewers" ? value.split(',').map(s => s.trim()) : value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+}
+
   return (
     <div className={style.container}>
-        <form className={style.form}>
+        <form onSubmit={handleSubmit} className={style.form}>
         <label>
             Project name
-            <input className={style.input} type="text" name="projectName" required />
+            <input className={style.input} onChange={handleFormChange} value={formData.projectName} type="text" name="projectName" required />
         </label>
         <label>
             Url
-            <input className={style.input} type="url" name="url" required />
+            <input className={style.input} onChange={handleFormChange} value={formData.projectUrl} type="url" name="projectUrl" required />
+        </label>
+        <label>
+            Description
+            <input className={style.input} onChange={handleFormChange} value={formData.description} type="text" name="description" required />
         </label>
         <label>
             Reviewers (comma-separated emails)
-            <input className={style.input} type="text" name="reviewers" placeholder="email1@example.com, email2@example.com" />
+            <input className={style.input} onChange={handleFormChange} 
+            value={Array.isArray(formData.reviewers) ? formData.reviewers.join(', ') : ''}
+            type="text" name="reviewers" placeholder="email1@example.com, email2@example.com" />
         </label>
         <button type="submit">Submit</button>
         </form>
