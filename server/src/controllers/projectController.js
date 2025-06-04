1e05f8099003504a526dd5f52af8955ec6c81e24
arakiztain <arakiztain.gaizka@gmail.com>
2025-06-04 12:51:22 +0200

    reportIssue

--- server/src/controllers/projectController.js
+++ server/src/controllers/projectController.js
@@ -35,7 +35,7 @@ const createProject = async (req, res, next) => {
 
     const folderId = responseFolder.data.id;
 
-    const listNames = ["Copy Revision", "Design Issues", "Requested Change", "New Item"];
+    const listNames = ["copy revision", "design issues", "requested change", "new item"];
     const createdLists = [];
 
     for (const name of listNames) {
@@ -105,6 +105,7 @@ const getAllProjects = async (req, res, next) => {
     } else {
       projects = await projectModel.find({ user: req.user._id }).populate("users", "email");
       if (!projects || projects.length === 0) throw new NotFoundError("You have no projects");
+      //clickup llamada id
     }
 
     res.json(projects);
